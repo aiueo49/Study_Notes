b3dc91f43b3d210615fc18434dfcaaf735710c7e
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-07-15 18:22:42 +0900

    ✨ UseStateの使い方

--- React/モダンJavaScriptの基本から始める　React実践の教科書/Reactの基本/my-app/src/App.jsx
+++ React/モダンJavaScriptの基本から始める　React実践の教科書/Reactの基本/my-app/src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  // Stateの定義
+  const [num, setNum] = useState(0);
+
   // ボタンを押した時に実行する関数を定義
   const onClickButton = () => {
-    alert();
+    setNum((prev) => prev + 1);
   };
 
   return (
@@ -12,6 +16,7 @@ export const App = () => {
       <ColoredMessage color="blue">"お元気ですか？"</ColoredMessage>
       <ColoredMessage color="pink">"元気です！"</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
     </>
   );
 };
